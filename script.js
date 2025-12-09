7d278a8bd21bfb5f66d454df044d42ab4adf24a4
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2025-12-09 18:17:27 +0100

    #3 to be deleted maybe

---
+++ script.js
@@ -0,0 +1,18 @@
+// vyber burger a menu
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+// kliknutí na burger
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active'); // otevře/zavře menu
+    burger.classList.toggle('toggle');  // animace burger -> kříž
+});
+
+// zavření menu po kliknutí na odkaz
+const navLinks = document.querySelectorAll('.nav-links a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('nav-active');
+        burger.classList.remove('toggle');
+    });
+});
